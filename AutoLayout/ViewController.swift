6ec971e656cefe6bf033556a1fffc728fc8ef2ec
janezhu1618 <janezhu@pursuit.org>
2018-11-14 14:15:38 -0500

    first commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Jane Zhu on 11/14/18.
+//  Copyright © 2018 Jane Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
